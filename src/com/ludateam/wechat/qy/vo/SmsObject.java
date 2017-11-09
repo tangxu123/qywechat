b680bdf34bac1240166a86b9606b9a264f79f348
him <himkwok@126.com>
2017-11-09 13:48:36 +0800

    add sms object

--- src/com/ludateam/wechat/qy/vo/SmsObject.java
+++ src/com/ludateam/wechat/qy/vo/SmsObject.java
@@ -12,40 +12,22 @@ package com.ludateam.wechat.qy.vo;/*
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * Created by Him on 2017/11/8.
+ * Created by Him on 2017/11/9.
  */
 
+public class SmsObject {
+
+    private String rwId;
+    private String sjhm;
+    private String dxnr;
 
 
-public class SubmitRsp {
-    private String  success;
-    private String  rspcod;
-    private String  msgGroup;
-    private String  sjhm;
-    private String  rwId;
-
-    public String getSuccess() {
-        return success;
+    public String getRwId() {
+        return rwId;
     }
 
-    public void setSuccess(String success) {
-        this.success = success;
-    }
-
-    public String getRspcod() {
-        return rspcod;
-    }
-
-    public void setRspcod(String rspcod) {
-        this.rspcod = rspcod;
-    }
-
-    public String getMsgGroup() {
-        return msgGroup;
-    }
-
-    public void setMsgGroup(String msgGroup) {
-        this.msgGroup = msgGroup;
+    public void setRwId(String rwId) {
+        this.rwId = rwId;
     }
 
     public String getSjhm() {
@@ -56,11 +38,11 @@ public class SubmitRsp {
         this.sjhm = sjhm;
     }
 
-    public String getRwId() {
-        return rwId;
+    public String getDxnr() {
+        return dxnr;
     }
 
-    public void setRwId(String rwId) {
-        this.rwId = rwId;
+    public void setDxnr(String dxnr) {
+        this.dxnr = dxnr;
     }
 }
