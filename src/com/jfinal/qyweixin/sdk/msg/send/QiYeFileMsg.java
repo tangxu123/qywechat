20e863b28df1fc09dbef7ce156a7dcabba69d629
him <himkwok@126.com>
2018-05-17 12:19:11 +0800

    发送文件图片消息实体

--- src/com/jfinal/qyweixin/sdk/msg/send/QiYeFileMsg.java
+++ src/com/jfinal/qyweixin/sdk/msg/send/QiYeFileMsg.java
@@ -2,36 +2,47 @@ package com.jfinal.qyweixin.sdk.msg.send;
 
 /**
  * 文件消息
- * @author Javen
  *
+ * @author Javen
  */
 public class QiYeFileMsg extends QiYeBaseMsg {
-	private String media_id;
+    private String media_id;
 
-	public String getMedia_id() {
-		return media_id;
-	}
+    public String getMedia_id() {
+        return media_id;
+    }
 
-	public void setMedia_id(String mediaId) {
-		media_id = mediaId;
-	}
+    public void setMedia_id(String mediaId) {
+        media_id = mediaId;
+    }
 
-	public QiYeFileMsg(String mediaId) {
-		media_id = mediaId;
-		this.msgtype=MessageType.file.name();
-	}
-	
-	public QiYeFileMsg() {
-		this.msgtype=MessageType.file.name();
-	}
+    public QiYeFileMsg(String mediaId) {
+        media_id = mediaId;
+        this.msgtype = MessageType.file.name();
+    }
 
-	private  SendFile file ;
+    public QiYeFileMsg() {
+        this.msgtype = MessageType.file.name();
+    }
 
-	public SendFile getFile() {
-		return file;
-	}
+    private SendFile file;
 
-	public void setFile(SendFile file) {
-		this.file = file;
-	}
+    public SendFile getFile() {
+        return file;
+    }
+
+    public void setFile(SendFile file) {
+        this.file = file;
+    }
+
+
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
--- src/com/jfinal/qyweixin/sdk/msg/send/QiYeImageMsg.java
+++ src/com/jfinal/qyweixin/sdk/msg/send/QiYeImageMsg.java
@@ -2,26 +2,48 @@ package com.jfinal.qyweixin.sdk.msg.send;
 
 /**
  * 图片消息
- * @author Javen
  *
+ * @author Javen
  */
 public class QiYeImageMsg extends QiYeBaseMsg {
-	private String media_id;
+    private String media_id;
 
-	public String getMedia_id() {
-		return media_id;
-	}
+    public String getMedia_id() {
+        return media_id;
+    }
 
-	public void setMedia_id(String mediaId) {
-		media_id = mediaId;
-	}
+    public void setMedia_id(String mediaId) {
+        media_id = mediaId;
+    }
 
-	public QiYeImageMsg(String mediaId) {
-		media_id = mediaId;
-		this.msgtype=MessageType.image.name();
-	}
-	public QiYeImageMsg() {
-		this.msgtype=MessageType.image.name();
-	}
-	
+    public QiYeImageMsg(String mediaId) {
+        media_id = mediaId;
+        this.msgtype = MessageType.image.name();
+    }
+
+    public QiYeImageMsg() {
+        this.msgtype = MessageType.image.name();
+    }
+
+    private QiYeImage image;
+
+    public QiYeImage getImage() {
+        return image;
+    }
+
+    public void setImage(QiYeImage image) {
+        this.image = image;
+    }
+
+    private String msgtype;
+
+    @Override
+    public String getMsgtype() {
+        return "image";
+    }
+
+    @Override
+    public void setMsgtype(String msgtype) {
+        this.msgtype = msgtype;
+    }
 }
